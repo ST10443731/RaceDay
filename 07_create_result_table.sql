5a1409cee5a86a60aa65417acd4d6cafaa142d65
ST10443731 <steo443731@rcconnect.edu.za>
2026-09-03 00:26:02 +0200

    Add Result Table

---
+++ 07_create_result_table.sql
@@ -0,0 +1,8 @@
+Create Table Result(
+ResultId INT IDENTITY(1,1) PRIMARY KEY,
+EnrolmentId INT NOT NULL UNIQUE,
+FinidhTime TIME NOT NULL,
+Position INT NOT NULL,
+RecordedAt DATETIME NOT NULL DEFAULT GETDATE(),
+CONSTRAINT FK_Result_Enrolment FOREIGN KEY (EnromentId) REFERENCES Enrolment(EnrolmentId)
+)
