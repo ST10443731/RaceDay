e4ab8daf3bc31d66827c593a92b644b80bce00c6
ST10443731 <steo443731@rcconnect.edu.za>
2026-09-03 01:10:58 +0200

    Add sample organizer and participant to database

--- 08_insert_role_table.sql
+++ 08_insert_role_table.sql
@@ -1,6 +1,7 @@
-INSERT INTO ROLE (RoleName) VALUES ('Organizer'),('Participant');
-INSERT INTO [User] (FullName, Email, PasswordHash,RoleId) VALUES
-('Tau Mokwena', 'mokwenaT@mail,co,za', 'HASH_PLACEHOLDER_1', (SELECT ROLEId FROM ROLE WHERE RoleName ='Organizer')),
-('Lindiwe Dlamini', 'liddy@mail.co.za', 'HASH_PLACEHOLDER_2', (SELECT ROLEId FROM ROLE WHERE RoleName = 'Organizer')),
-('Thabiso Sebopa', 'Lazzy@mail.co.za', 'HASH_PLACEHOLDER_3', (SELECT ROLEId FROM ROLE WHERE RoleName = 'Participant')),
-('Lesego Maputla', 'Putla@mail.co.za', 'HASH_PLACEHOLDER_4', (SELECT ROLEId FROM ROLE WHERE RoleName = ' participant'))
+INSERT INTO [User] (FullName, Email, PasswordHash, RoleId) VALUES
+('Tau Mokwena', 'mokwenaT@mail.co.za', 'HASH_PLACEHOLDER_1', (SELECT RoleId FROM Role WHERE RoleName = 'Organizer')),
+('Lindiwe Dlamini', 'liddy@mail.co.za', 'HASH_PLACEHOLDER_2', (SELECT RoleId FROM Role WHERE RoleName = 'Organizer')),
+('Thabiso Sebopa', 'Lazzy@mail.co.za', 'HASH_PLACEHOLDER_3', (SELECT RoleId FROM Role WHERE RoleName = 'Participant')),
+('Lesego Maputla', 'Putla@mail.co.za', 'HASH_PLACEHOLDER_4', (SELECT RoleId FROM Role WHERE RoleName = 'Participant'));
+
+SELECT * FROM [User];
