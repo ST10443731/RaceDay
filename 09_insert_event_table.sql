5a454680f5e7177d72337a0d9002dae408087441
ST10443731 <steo443731@rcconnect.edu.za>
2026-09-03 01:33:53 +0200

    add infomation into event table

---
+++ 09_insert_event_table.sql
@@ -0,0 +1,4 @@
+INSERT INTO Event(OgernizerId, Name, Description, EnetDate,Location, RouteInfo) VALUES
+(1, 'Johannesburg City Marathon', 'Annual road marathon through the johanesburg CBD.' , '2026-10-18 06:00:00' , 'Johannesburg, Gauteng' , 'Starts at Mary Fitzegarald Squre, go through braamfontein .'),
+(2, 'Soweto Fun Run', 'Community fun run supporting local charities.', '2026-09-27 07:00:00', 'Soweto, Gauteng', 'Starts and ends at the Soweto Theatre.'),
+(2, 'Cape Town Cycle Challange', 'cycling event along camps bay.' , '2026-06-02 06:23:00', 'Cape Town, Western Cape' , 'Coastal Route and camps bay.')
