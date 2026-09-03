ff66654f0e6ad03ad77afcb057e9d113d46b23c9
ST10443731 <steo443731@rcconnect.edu.za>
2026-09-03 15:57:28 +0200

    Adding a participant

--- 08_insert_role_table.sql
+++ 08_insert_role_table.sql
@@ -2,6 +2,6 @@ INSERT INTO [User] (FullName, Email, PasswordHash, RoleId) VALUES
 ('Tau Mokwena', 'mokwenaT@mail.co.za', 'HASH_PLACEHOLDER_1', (SELECT RoleId FROM Role WHERE RoleName = 'Organizer')),
 ('Lindiwe Dlamini', 'liddy@mail.co.za', 'HASH_PLACEHOLDER_2', (SELECT RoleId FROM Role WHERE RoleName = 'Organizer')),
 ('Thabiso Sebopa', 'Lazzy@mail.co.za', 'HASH_PLACEHOLDER_3', (SELECT RoleId FROM Role WHERE RoleName = 'Participant')),
-('Lesego Maputla', 'Putla@mail.co.za', 'HASH_PLACEHOLDER_4', (SELECT RoleId FROM Role WHERE RoleName = 'Participant'));
+('Lesego Maputla', 'Putla@mail.co.za', 'HASH_PLACEHOLDER_4', (SELECT RoleId FROM Role WHERE RoleName = 'Participan'));
 
 SELECT * FROM [User];
